25659da654135d7eb8e7ea7aa8007f906dae8425
Salman A <salman.a@hatio.in>
2021-08-26 17:18:48 +0530

    Add theme context

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
-import "./App.css";
+import "./App.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import NavBar from "./components/NavBar";
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext(
+  {} as {
+    darkMode: boolean;
+    setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
+  }
+);
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const className = "container " + (darkMode ? "dark" : "light");
+
   return (
-    <div className="App">
-      <Router>
-        <NavBar />
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      <div className={className}>
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
