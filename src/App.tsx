a401ebdae80f81b69ec37745e35cbc7c84fd37b3
Salman A <salman.a@hatio.in>
2021-08-26 11:46:24 +0530

    Add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
