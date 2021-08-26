26e05e64d4cc118a2a01916e62bb8e5d55ac4e45
Salman A <salman.a@hatio.in>
2021-08-26 17:17:52 +0530

    Add dark mode toggle

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { ThemeContext } from "../App";
 import "./NavBar.scss";
 
 const NavBar: React.FC = () => {
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
   return (
-    <nav>
+    <nav className={darkMode ? "dark" : "light"}>
       <ul>
         <li className="logo">Our Blog</li>
         <img
-          src="https://img.icons8.com/color/48/000000/google-blog-search.png"
+          src="https://img.icons8.com/color/96/000000/google-blog-search.png"
           alt="an icon of a blog"
         />
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li style={{ marginRight: "2rem" }}>
+        <li>
           <Link to="/about">About</Link>
+        </li>
+        <li style={{ marginRight: "2rem" }}>
+          <label className="switch">
+            <input
+              type="checkbox"
+              value={String(darkMode)}
+              onChange={() => setDarkMode(!darkMode)}
+            />
+            <span className="slider round"></span>
+          </label>
         </li>
       </ul>
     </nav>
