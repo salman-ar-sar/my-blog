ec7496af249b6bbe753de56ba19919b3ab473cd4
Salman A <salman.a@hatio.in>
2021-08-27 17:44:01 +0530

    Add routing for login and import contexts

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,38 @@ import "./App.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import NavBar from "./components/NavBar";
-import { createContext, useState } from "react";
+import { useState } from "react";
 import ArticlePage from "./pages/Article";
 import Profile from "./pages/Profile";
-
-export const ThemeContext = createContext(
-  {} as {
-    darkMode: boolean;
-    setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
-  }
-);
+import { LoginContext, ThemeContext } from "./components/Contexts";
+import Login from "./pages/Login";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
+  const [user, setUser] = useState("");
 
   return (
-    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
-      <div className={"container " + (darkMode ? "dark" : "light")}>
-        <Router>
-          <NavBar />
-          <Switch>
-            <Route exact path="/">
-              <HomePage />
-            </Route>
-            <Route path="/article/:id" component={ArticlePage} />
-            <Route path="/profile">
-              <Profile />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
-    </ThemeContext.Provider>
+    <LoginContext.Provider value={{ user, setUser }}>
+      <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+        <div className={"container " + (darkMode ? "dark" : "light")}>
+          <Router>
+            <NavBar />
+            <Switch>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route path="/article/:id" component={ArticlePage} />
+              <Route path="/profile">
+                <Profile />
+              </Route>
+              <Route path="/login">
+                <Login />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      </ThemeContext.Provider>
+    </LoginContext.Provider>
   );
 }
 
