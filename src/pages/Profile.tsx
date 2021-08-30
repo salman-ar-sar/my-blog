23b4c607a41d492dddb928956a4dc717bcc0bdd9
Salman A <salman.a@hatio.in>
2021-08-30 12:44:53 +0530

    Add profile details sidebar

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,11 +1,14 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
+import { Link } from "react-router-dom";
+import { ThemeContext } from "../components/Contexts";
 import { Article } from "../types/article";
-import ArticleContainer from "./ArticleContainer";
+import ArticleContainer from "../components/ArticleContainer";
 import "./Profile.scss";
 
 const Profile = () => {
   const [{ user }] = useCookies(["user"]);
+  const { darkMode } = useContext(ThemeContext);
   const [articles, setArticles] = useState<Article[]>();
 
   const fetchData = useCallback(() => {
@@ -27,7 +30,26 @@ const Profile = () => {
   return (
     <div className="profilePage">
       {user ? <h2>Welcome {user}!</h2> : <h2>Not logged in!</h2>}
-      {articles && <ArticleContainer articles={articles} />}
+      <div className="profileContainer">
+        {articles && <ArticleContainer articles={articles} />}
+        {user && (
+          <div className="profileDeets">
+            <img
+              className="userImage"
+              src="https://image.flaticon.com/icons/png/512/1077/1077012.png"
+              alt="user"
+            />
+            <h2 className="userName">{user}</h2>
+            <p>145 followers</p>
+            <p>{articles?.length} articles!</p>
+            <div className="inputClass">
+              <Link to="/post">
+                <button className={darkMode ? "dark" : ""}>New Post +</button>
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
