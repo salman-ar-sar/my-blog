f06cee90f61098c125f4ad4fa4ea298a875d8751
Salman A <salman.a@hatio.in>
2021-08-30 11:10:07 +0530

    Remove login context

--- src/components/Contexts.tsx
+++ src/components/Contexts.tsx
@@ -1,9 +1,5 @@
 import { createContext } from "react";
 
-export const LoginContext = createContext(
-  {} as { user: string; setUser: React.Dispatch<React.SetStateAction<string>> }
-);
-
 export const ThemeContext = createContext(
   {} as {
     darkMode: boolean;
