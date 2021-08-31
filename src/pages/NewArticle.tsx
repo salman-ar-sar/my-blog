2808b49ddcb9b89c3d9592b09d56252e83f7f16d
Salman A <salman.a@hatio.in>
2021-08-31 11:57:43 +0530

    Fix content being single paragraph

--- src/pages/NewArticle.tsx
+++ src/pages/NewArticle.tsx
@@ -13,21 +13,35 @@ const NewArticle = () => {
   const { darkMode } = useContext(ThemeContext);
   const history = useHistory<State>();
 
+  type FormData = Article & {
+    contents?: string;
+  };
+
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<Article>();
+  } = useForm<FormData>();
 
   const submitForm = handleSubmit(async (article) => {
+    console.log("in");
+
     article.id = Math.ceil(Math.random() * 10);
+
     let spacelessString: string = article.title
       .toLowerCase()
       .trim()
       .replace(/\s/g, "-");
+
     article.name = spacelessString.replace(/[^a-z0-9-]/g, "");
     article.author = "Enid Blyton";
+    if (article.contents) {
+      article.content = article.contents.split("\n");
+    }
+    delete article.contents;
+
     // console.log(JSON.stringify(article));
+
     const response = await fetch("http://localhost:8000/articles", {
       method: "post",
       body: JSON.stringify(article),
@@ -65,16 +79,16 @@ const NewArticle = () => {
         </div>
         {errors.title && <div className="errorMsg">This field is required</div>}{" "}
         <div className="inputClass">
-          <label htmlFor="content">Content: </label>
+          <label htmlFor="contents">Content: </label>
           <textarea
             className={darkMode ? "dark" : ""}
-            {...register("content", { required: true })}
-            name="content"
+            {...register("contents", { required: true })}
+            name="contents"
             cols={30}
             rows={10}
           ></textarea>
         </div>
-        {errors.content && (
+        {errors.contents && (
           <div className="errorMsg">This field is required</div>
         )}{" "}
         <div className="inputClass">
