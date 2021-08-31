ae4faad464c7e79b12f6905a74fa6730b2258fe0
Salman A <salman.a@hatio.in>
2021-08-31 11:20:59 +0530

    Add delete button

--- src/components/ArticleContainer.tsx
+++ src/components/ArticleContainer.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useHistory, useLocation } from "react-router-dom";
 import { Article } from "../types/article";
 import "./ArticleContainer.scss";
 
@@ -8,6 +8,23 @@ interface Props {
 
 const ArticleContainer = ({ articles }: Props) => {
   let location = useLocation();
+  const history = useHistory();
+
+  const deleteArticle = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    id: number
+  ) => {
+    event.preventDefault();
+    const response = await fetch(`http://localhost:8000/articles/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      history.go(0);
+    } else {
+      alert(response);
+    }
+  };
 
   return (
     <div className="articleContainer">
@@ -21,7 +38,11 @@ const ArticleContainer = ({ articles }: Props) => {
               src={`https://picsum.photos/300/200?random=${article.id}`}
               alt="article"
             />
-            {location.pathname === "/profile" && <button>Delete</button>}
+            {location.pathname === "/profile" && (
+              <button onClick={(event) => deleteArticle(event, article.id)}>
+                Delete
+              </button>
+            )}
           </div>
         );
       })}
