9d4b6f18155dbd42a58740ff74317b65bb31c22f
Salman A <salman.a@hatio.in>
2021-09-03 12:59:41 +0530

    Show articles of an author

--- src/pages/ViewProfile.tsx
+++ src/pages/ViewProfile.tsx
@@ -1,6 +1,7 @@
 import { match } from "react-router-dom";
+import ArticleContainer from "../components/ArticleContainer";
 import useFetch from "../components/useFetch";
-import { User } from "../types/types";
+import { Article, User } from "../types/types";
 
 interface Params {
   name: string;
@@ -11,15 +12,33 @@ const ViewProfile = ({ match }: { match: match<Params> }) => {
 
   const {
     data: user,
-    isPending,
-    errorMsg,
+    isPending: isPending1,
+    errorMsg: errorMsg1,
   } = useFetch<User>(`http://localhost:8000/users/${name}`);
+
+  const {
+    data: articles,
+    isPending: isPending2,
+    errorMsg: errorMsg2,
+  } = useFetch<Article[]>(
+    `http://localhost:8000/articles?author=${user?.name
+      .trim()
+      .replace(" ", "%20")}`
+  );
+
+  const isPending = isPending1 || isPending2;
+  const errorMsg = errorMsg1 || errorMsg2;
 
   return (
     <div>
       <h1>{user?.name}</h1>
       {isPending && <div className="loadingMsg">Loading...</div>}
       {errorMsg && <div className="errorMsg">{errorMsg}</div>}
+      {user && (
+        <div className="articleContainer">
+          {articles && <ArticleContainer articles={articles} />}
+        </div>
+      )}
     </div>
   );
 };
