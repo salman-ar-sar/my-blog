c5f5863054852fafe4efb6e555e7df1e4c4257ce
Salman A <salman.a@hatio.in>
2021-09-03 10:59:37 +0530

    Add register page

---
+++ src/pages/Register.tsx
@@ -0,0 +1,86 @@
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
+import { ThemeContext } from "../components/Contexts";
+import { useCookies } from "react-cookie";
+import "./Login.scss";
+import sha256 from "../components/sha256";
+import { User, UserForm } from "../types/types";
+
+const Register = () => {
+  const history = useHistory();
+  const { darkMode } = useContext(ThemeContext);
+  const [, setCookie] = useCookies(["user"]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserForm>();
+
+  const submitForm = handleSubmit(async ({ id, password, name }) => {
+    let user = {} as User;
+
+    user.id = id;
+    user.passwordHash = await sha256(password);
+    user.name = name;
+
+    const response = await fetch("http://localhost:8000/users", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    });
+
+    if (response.ok) {
+      setCookie("user", user.id, { path: "/" });
+      history.push("/profile");
+    } else {
+      alert(response);
+    }
+  });
+
+  return (
+    <div className="loginForm">
+      <h2>Register</h2>
+      <hr />
+      <form onSubmit={submitForm}>
+        <div className="inputClass">
+          <label htmlFor="name">Name: </label>
+          <input
+            {...register("name", { required: true })}
+            type="text"
+            name="name"
+          />
+        </div>
+        {errors.name && <div className="errorMsg">This field is required</div>}{" "}
+        <div className="inputClass">
+          <label htmlFor="id">Username: </label>
+          <input
+            {...register("id", { required: true })}
+            type="text"
+            name="id"
+          />
+        </div>
+        {errors.id && <div className="errorMsg">This field is required</div>}{" "}
+        <div className="inputClass">
+          <label htmlFor="password">Password: </label>
+          <input
+            {...register("password", { required: true })}
+            type="password"
+            name="password"
+          />
+        </div>
+        {errors.password && (
+          <div className="errorMsg">This field is required</div>
+        )}{" "}
+        <div className="inputClass">
+          <button className={darkMode ? "dark" : ""}>Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
