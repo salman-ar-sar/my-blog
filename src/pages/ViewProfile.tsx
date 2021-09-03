e04ba2f9b36d86f0e995dd1e319bb5e175044a3a
Salman A <salman.a@hatio.in>
2021-09-03 12:38:36 +0530

    Add name heading in profile page

--- src/pages/ViewProfile.tsx
+++ src/pages/ViewProfile.tsx
@@ -1,13 +1,27 @@
 import { match } from "react-router-dom";
+import useFetch from "../components/useFetch";
+import { User } from "../types/types";
 
 interface Params {
   name: string;
 }
 
 const ViewProfile = ({ match }: { match: match<Params> }) => {
-  console.log(match.params.name);
+  const name = match.params.name;
 
-  return <div></div>;
+  const {
+    data: user,
+    isPending,
+    errorMsg,
+  } = useFetch<User>(`http://localhost:8000/users/${name}`);
+
+  return (
+    <div>
+      <h1>{user?.name}</h1>
+      {isPending && <div className="loadingMsg">Loading...</div>}
+      {errorMsg && <div className="errorMsg">{errorMsg}</div>}
+    </div>
+  );
 };
 
 export default ViewProfile;
